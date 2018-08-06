1912cb745023bfceae713d144ba103b5f21ee0d2
Olivier <olivier.fradette.roy@gmail.com>
2018-08-06 15:09:15 -0400

    test

--- src/ShowList.java
+++ src/ShowList.java
@@ -1,5 +1,7 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import static java.lang.System.out;
@@ -7,20 +9,34 @@ import static java.lang.System.out;
 public class ShowList<ShowNode> {
 
 private ShowNode header;
-private ShowNode size;
+private int size;
 
 public ShowList(){
     List<ShowList> showNodeList = new ArrayList<>();
 }
 
-public ShowList copy(ShowList showList){
+/*public ShowList copy(ShowList showList){
     showList = clone("second test");
+}*/
+
+public void insertAtIndex(TVShow tvShow, int index ) throws NoSuchElementException{
+if (index <-1 || index>size-1){
+    throw new NoSuchElementException();
+}
+    ShowList.ShowNode newElement = new ShowNode(tvShow, );
+}
+
+public void addToStart (TVShow tvShow){
+
+    ShowList.ShowNode newElement = new ShowNode(tvShow, );
+    newElement.add(1, )
+
 }
 
     public class ShowNode {
 
         private TVShow tvShow;
-        private ShowNode showNode;
+        private PointShowNode showNode;
 
         public ShowNode(){
             tvShow=null;
